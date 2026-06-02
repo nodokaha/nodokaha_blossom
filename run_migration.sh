0777d42f9707d72bd19d6dfc9c6408fb12bfc9b8
nodokaha <scpeee@wail.ch>
2026-06-03 08:00:33 +0900

    delete

--- run_migration.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-cd /mnt/c/Users/genne/nodokaha_blossom-1.worktrees/agents-online-sandbox-game-design-symfony
-docker compose exec -T app php bin/console doctrine:migrations:migrate --no-interaction
